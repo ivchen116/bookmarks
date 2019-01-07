d8365211f685b919b4ce120e15d2720a167f0a27
ivchen116 <doner@foxmail.com>
2019-01-07 21:27:41 +0800

    调整页面显示

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from logging.handlers import RotatingFileHandler
+from flask_moment import Moment
 
 from config import Config
 
@@ -11,6 +12,7 @@ app = Flask(__name__)
 app.config.from_object(Config)
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+moment = Moment(app)
 login = LoginManager(app)
 login.login_view = 'auth.login'
 
