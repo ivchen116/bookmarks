27ce6704ca6d57be96479e8d3db20b0942f0557a
ivchen116 <doner@foxmail.com>
2019-06-01 17:37:14 +0800

    将已读添加到未读列表

--- app/static/js/item.js
+++ app/static/js/item.js
@@ -12,6 +12,45 @@ function itemLoadPara(filter, collection) {
 function itemLoadMore() {
 	itemLoadSpec(items_have_get, items_per_page, 'queue', '')
 }
+function itemBuildNew(item) {
+	var html_template = '<div class="item item_type_normal"><div class="item_content"><a class="item_link start_articleview" href="#" target="_blank"/><a class="title" href="#"> </a><span class="thumb"> </span><ul class="sub clearfix"><li class="original_url_container"><a class="original_url" href="#" target="_blank"> </a></li></ul><div class="clear"/><ul class="buttons"></ul></div></div>';
+	var $item = $(html_template)
+	$item.attr('id', item.itemid);
+	$item.find('.item_link').attr("href", item.given_url);
+
+	var dis_title = item.given_title?item.given_title:item.resolved_title;
+	dis_title = dis_title?dis_title:item.given_url;
+	$item.find('.title').text(dis_title).attr("href", item.given_url);
+	$item.find('.thumb').css("background-image","url(/static/image/direct.jpg)");
+	$item.find('.original_url').text(item.netloc).attr("href", item.given_url);
+	
+	var $buttons = $item.find('.buttons')
+	if (item.archived) {
+		$buttons.append(
+				$('<li><a href="#">标记为未读</a></li>')
+					.addClass('action_mark action_mark_archived')
+					.attr('title', '标记为未读'));
+	} else {
+		$buttons.append(
+			$('<li><a href="#">完成</a></li>')
+				.addClass('action_mark')
+				.attr('title', '完成'));
+	}
+	$buttons.append(
+			$('<li><a href="#" data-toggle="delete-confirmation">删除</a></li>')
+				.addClass('action_delete')
+				.attr('title', '删除'));
+	$buttons.append(
+			$('<li><a href="#">添加收藏夹</a></li>')
+				.addClass('action_tag')
+				.attr('title', '添加收藏夹'));
+	$buttons.append(
+			$('<li><a href="#">标记为精华</a></li>')
+				.addClass('action_favorite')
+				.attr('title', '标记为精华'));
+
+	return $item;
+}
 function itemLoadSpec(offset, count, state, collection) {
 	items_is_loading = true;
 	$("#btn-loading").attr("disabled","disabled");
@@ -24,11 +63,7 @@ function itemLoadSpec(offset, count, state, collection) {
 		success: function(data) {
 			if (data.status == '1') {
 				$.each(data.lists, function(i, item) {
-					var item_template="<div class=\"item item_type_normal\" id=\"{id}\"><div class=\"item_content\"><a class=\"item_link start_articleview\" href=\"{url}\" target=\"_blank\"></a><a class=\"title\" href=\"{url}\">{title}</a><span class=\"thumb\" style=\"background-image: url(/static/image/direct.jpg)\"> </span><ul class=\"sub clearfix\"><li class=\"original_url_container\"><a class=\"original_url\" href=\"{url}\" target=\"_blank\">{netloc}</a></li><li class=\"tags hasTags\"><span class=\"tag_container\"><a class=\"tag\" href=\"/tags/abcd\">标签</a></span></li></ul><ul class=\"clearfix\" style=\"display:none\"><li class=\"author\">{author}</li></ul><div class=\"clear\"></div><ul class=\"buttons\"><li class=\"action_mark\" title=\"完成\"><a href=\"#\">删除</a></li><li class=\"action_delete\" title=\"删除\"><a href=\"#\"  data-toggle=\"delete-confirmation\">删除</a></li><li class=\"action_tag\" title=\"添加收藏夹\"><a href=\"#\">添加收藏夹</a></li><li class=\"action_favorite \" title=\"标记为精华\"><a href=\"#\">标记为精华</a></li></ul></div></div> ";
-					var dis_title = item.given_title?item.given_title:item.resolved_title;
-					dis_title = dis_title?dis_title:item.given_url;
-					var item_html=item_template.format({id:item.itemid, url:item.given_url, title:dis_title, netloc:item.netloc, author: item.author});
-					$(".page_queue_list").append(item_html);
+					$(".page_queue_list").append(itemBuildNew(item));
 				});
 				items_have_get += data.lists.length
 				if (data.lists.length < items_per_page) {
@@ -54,11 +89,12 @@ $(document).ready(function(){
 	
 	$('.page_queue_list').on('click', '.action_mark a', function(e){
 		var item = $(this).closest('.item')
+		var archive = $(this).parent('.action_mark_archived').length>0?0:1;
 		
 		$.ajax({    
 			url: "/item/archive",    
 			dataType: "json", 
-			data: { id: item.attr("id"), done: 1 },        
+			data: { id: item.attr("id"), done: archive },        
 			type: "POST",    
 			context: item,
 			success: function(data) {
